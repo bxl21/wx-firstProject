786cfe00a55c300d60ddc6e6781df803cfa24e44
bxl21 <xiaolinbi@creditease.cn>
2018-08-31 17:55:48 +0800

    新增登录功能页

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,29 @@
 Page({
-
+  data:{
+    mobile:'',
+    password:''
+  },
+  bindMobile:function(e){
+    this.mobile = e.detail.value,
+      console.info(this.mobile)
+  },
+  bindPassword:function(e){
+    this.password = e.detail.value
+  },
+  sendData: function (e) {
+    wx.request({
+      url: 'http://10.143.253.15:30106/api/h5/auth/smsSend',
+      data: {
+        mobile: this.mobile,
+      },
+      header: { 'Content-Type': 'application/json'},
+      method: 'POST',
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) { console.info(res.statusCode) ,
+      console.info(res.data)},
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  }
 })
--- pages/myself/myself.js
+++ pages/myself/myself.js
@@ -1,4 +1,4 @@
-
+const date = new Date()
 Page({
   data: {
     objectArray: [
